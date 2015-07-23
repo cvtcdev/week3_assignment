0fe81a0629f7e512c636d19ec132cbd91fa08e29
Edward Dumholt <edumholt@student.cvtc.edu>
2015-07-23 15:56:41 -0500

    render_page complete

--- lib/SubmitApp.rb
+++ lib/SubmitApp.rb
@@ -1,20 +1,25 @@
 class SubmitApp
   def call(env)
+    request = Rack::Request.new(env)
+    name = request.POST["name"]
+    email = request.POST["email"]
+    message = request.POST["message"]
+
+    content = render_page(email, message, name)
+
+    [200, {"Content-type" => "text/html"}, [content]]
+  end
+
+  def render_page(email, message, name)
     content = ""
     content << File.open("htm/header.html").read
     content << "Name: "
-
-    request = Rack::Request.new(env)
-    name = request.POST["name"]
     content << name
     content << "<br>Email: "
-    email = request.POST["email"]
     content << email
     content << "<br>Message: <br>"
-    message = request.POST["message"]
     content << message
     content << File.open("htm/footer.html").read
-
-    [200, {"Content-type" => "text/html"}, [content]]
+    content
   end
 end
